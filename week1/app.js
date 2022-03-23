2f2dafdcaebd2e533d69ae055ecc1af82620961e
Shannon Ewing-Sudds <91341099+Shannon-ewingsudds@users.noreply.github.com>
2022-03-23 13:09:45 -0400

    Add files via upload

---
+++ week1/app.js
@@ -0,0 +1,7 @@
+const mathObj = require("./math");
+
+
+console.log("Sum: " + mathObj.sum(10, 2)); //12
+console.log("Subtract: " + mathObj.subtract(10, 2)); //8
+console.log("Divide: " + mathObj.divide(10, 2)); //5
+console.log("Multiply: " + mathObj.multiply(10, 2)); //20
---
+++ week1/math.js
@@ -0,0 +1,20 @@
+const sum = (a, b) => {
+    return a + b
+};
+
+const subtract = (c, d) => {
+    return c - d
+};
+const divide = (c, d) => {
+    return c / d
+};
+const multiply = (c, d) => {
+    return c * d
+};
+
+module.exports = {
+    sum,
+    subtract,
+    divide,
+    multiply
+};
