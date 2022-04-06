20b1358d85b3c24cce952d77c97c06ec1e3e2579
shanon-ewingsudds <shannon.ewing-sudds@bryanuniversity.edu>
2022-04-05 22:39:18 -0400

    working on week 3

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+const recycledItems = require('./routes/recycledItems');
 
 const PORT = 3000;
 
-let users = [
-	{name: 'Patrick', location: 'bushwick'},
-	{name: 'Ben', location: 'crown heights'},
-	{name: 'Anna', location: 'manhattan'},
-	{name: 'Casey', location: 'phoenix'},
-	{name: 'Fiji', location: 'phoenix'}
+//middleware
+app.use(express.json());
 
-];
+//route
+app.use('/itemsIntake', recycledItems);
 
-app.get('/users', (req, res) => {
-	res.send(users)
-});
-
-app.listen(PORT, ()=> {
-	console.log(`App startedon port: ${PORT}`)
+//server start-up logic
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
 });
